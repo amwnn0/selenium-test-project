8de5238d51577e836e7e5683204390324d226bc0
Aleksandr_K <amwnn@mail.ru>
2025-07-14 00:11:51 +0300

    test: add page object for login page

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from .base_page import BasePage
+from .main_page import MainPage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        current_url = self.browser.current_url
+        # реализуйте проверку на корректный url адрес
+        assert "login" in current_url, "URL must contain 'login'"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form must be present."
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form must be present."
